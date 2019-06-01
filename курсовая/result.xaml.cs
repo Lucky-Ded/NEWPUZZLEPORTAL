a17397f9a487e662738b91e4cf2020fd2121e7e9
LinaPol <LinaPol>
2019-06-01 13:41:28 +0700

    firsr

---
+++ курсовая/result.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace курсовая
+{
+    /// <summary>
+    /// Логика взаимодействия для result.xaml
+    /// </summary>
+    public partial class result : Window
+    {
+       
+        
+        public result()
+        {
+            InitializeComponent();
+            FileStream file1 = new FileStream(@"C:\ri87\date1.txt", FileMode.Open); //создаем файловый поток 
+            StreamReader reader = new StreamReader(file1); // создаем «потоковый читатель» и связываем его с файловым потоком 
+            list.Items.Add(reader.ReadToEnd()); //считываем все данные с потока и выводим на экран
+          
+        }
+
+        private void B7_Click(object sender, RoutedEventArgs e)
+        {
+            Window menu = new menu();
+            menu.Show();
+            this.Close();
+        }
+
+        
+    }
+}
---
+++ курсовая/winner.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace курсовая
+{
+    /// <summary>
+    /// Логика взаимодействия для winner.xaml
+    /// </summary>
+    public partial class winner : Window
+    {
+        public winner()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            menu menu = new menu();
+            File.AppendAllText(@"C:\ri87\date1.txt",name.Text );
+            File.AppendAllText(@"C:\ri87\date1.txt", "                  ");
+            File.AppendAllText(@"C:\ri87\date1.txt", w1.Content.ToString());
+            File.AppendAllText(@"C:\ri87\date1.txt", "\n");
+           
+            menu.Show();
+            this.Close();
+        }
+    }
+}
