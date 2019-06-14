0bdd1e5538e444c5993e22945f06329cc824d7a0
Lucky-Ded <vitas1pro1@gmail.com>
2019-06-14 15:44:11 +0700

    nadeus vse

--- курсовая/test.cs
+++ курсовая/test.cs
@@ -21,7 +21,7 @@ namespace курсовая
             [TestCase]
             public void logic()
             {
-                logic l = new logic(C, R);           
+                logic l = new logic(C, R);
                 bool p = true;
                 for (int i = 0; i < C; i++)
                     for (int j = 0; j < R; j++)
@@ -35,38 +35,76 @@ namespace курсовая
 
 
             [TestCase]
-            public void scramble()
+            public void scramble1()
             {
                 Random rng = new Random();
                 logic l = new logic(C, R);
                 bool p = false;
                 int n = 100;
+                int t;
                 l.scramble(n);
+
+
                 for (int i = 0; i < C; i++)
                     for (int j = 0; j < R; j++)
                     {
-                        l.img[i, j] = i + j * C;
-                        for (int q = 0; q < n; q++)
+                        //l.img[i, j] = i + j * C;
+                        
+                        for (int k = 0; k < (C * R)+1; k++)
                         {
-                            int x1 = rng.Next(C);
-                            int y1 = rng.Next(R);
-
-                            int x2 = rng.Next(C);
-                            int y2 = rng.Next(R);
-
-                            int t = l.img[x1, y1];
-                            l.img[x1, y1] = l.img[x2, y2];
-                            l.img[x2, y2] = t;
-
-
-                            if (l.img[i, j] != l.img[x2, y2]) { p = true; }
+                            t = l.img[i, j];
+                            if (l.img[i, j] != t) { p = false   ; } else { p = true; }
                             
                         }
-                        Assert.IsTrue(p);
+                        
                     }
+
+
+                Assert.IsTrue(p);
                 
+
             }
 
+
+            //не пеезаписывает
+
+
+            [TestCase]
+            public void scramble2()
+            {
+                Random rng = new Random();
+                logic l = new logic(C, R);
+                bool p = true;
+                bool d = true;
+                int n = 10;
+                int e = 0;
+                int t;
+                int y;
+                l.scramble(n);
+
+
+                for (int i = 0; i < C; i++)
+                    for (int j = 0; j < R; j++)
+                    {
+                       
+                      
+                        for (int k = 0; k < C * R; k++)
+                        {
+                            t = l.img[i, j];
+                            if (l.img[i, j] != t) { p = false;  } else { p = true; e++; }
+
+                        }
+
+                    }
+                y = (e / n)*100 ;
+                
+
+                Assert.IsTrue(y>50);
+
+
+            }
+        
+            // хотя бы половина цифр соит не на своих часятх
 
 
             [TestCase]
@@ -74,19 +112,42 @@ namespace курсовая
             {
                 logic l = new logic(C, R);
                 bool p = false;
-                int ii = 1;
-                int jj = 1;
-                int num = 1;
-                l.move(ii,jj,num);
-                l.img[ii, jj] = num;
+                //int ii = 1;
+                //int jj = 1;
+                //int num = 1;
                 for (int i = 0; i < C; i++)
                     for (int j = 0; j < R; j++)
                     {
-                        l.img[i, j] = i + j * C;
-                        if(l.img[i,j] != l.img[ii,jj]){ p = true; }
+                        int t1 = l.img[i, j];
+                        int t2 = l.get(i, j);
+
+                        l.move(i, j, i + j * C);
+
+                        if (l.img[i, j] == t2 && l.get(i, j) == t1)
+                            p = true;
+
+                        
+                    }
+
+
+                Assert.IsTrue(p);
+            }
+
+            [TestCase]
+            public void get()
+            {
+                logic l = new logic(C, R);
+                bool p = true;
+                for (int i = 0; i < C; i++)
+                    for (int j = 0; j < R; j++)
+                    {
+                        if (l.get(i, j) != i + j * C) { p = false; }
+
                     }
                 Assert.IsTrue(p);
             }
+
         }
     }
 }
+                
